6c137a5fba3998bb79c0b34706348bd168ffc18d
吴钦飞 <wuqinfei_csdn@163.com>
2021-04-04 23:42:01 +0800

    【icons】animate

---
+++ src/assistant-widget/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+
+import AssistantCtrl from './AssistantCtrl.vue';
+
+new Vue({
+    el: '#app',
+    render: h => h(AssistantCtrl),
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader')
 
-module.exports= {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
-      { test: /\.(png|jpe?g|gif)$/i, use: [ 'file-loader' ] },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+module.exports = {
+    // entry: './src/index.js',
+    entry: './src/assistant-widget/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {test: /\.vue$/, loader: 'vue-loader'},
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            {test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']},
+            {test: /\.(png|jpe?g|gif|svg)$/i, use: ['file-loader']},
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // template: './src/index.html'
+            template: './src/assistant-widget/index.html'
+        }),
+        new VueLoaderPlugin()
+    ]
 };
